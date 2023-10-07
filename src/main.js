3cbe5ffcb9305693ff80567db21259daa41c66e4
bossth192 <mju6504101307@mju.ac.th>
2023-10-07 23:39:30 +0700

    แก้ไขcode

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./assets/tailwind.css";
 
-const app = createApp(App);
 
-app.use(router);
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-app.mount("#app");
+import App from './App.vue'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {bootstrap} from 'bootstrap/dist/js/bootstrap'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(bootstrap)
+app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,37 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import CityView from "../views/CityView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import HeadView from '../components/HeadView.vue'
+import cardView from '../components/cardView.vue'
+
+import WeartherView from '../components/WeartherView.vue'
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
-      meta: {
-        title: "Home",
-      },
+      path: '/',
+      name: 'main',
+      component: HomeView
     },
     {
-      path: "/weather/:state/:city",
-      name: "cityView",
-      component: CityView,
-      meta: {
-        title: "Weather",
-      },
+      path: '/HeadView',
+      name: 'HeadView',
+      component: HeadView
     },
-  ],
-});
+    {
+      path: '/cardView',
+      name: 'cardView',
+      component: cardView
+    }
+    ,
+    {
+      path: '/WeatherView',
+      name: 'WeartherView',
+      component: WeartherView
+    }
+  ]
+})
 
-router.beforeEach((to, from, next) => {
-  document.title = `${
-    to.params.state
-      ? `${to.params.city}, ${to.params.state}`
-      : to.meta.title
-  } | The Local Weather`;
-  next();
-});
-
-export default router;
+export default router
